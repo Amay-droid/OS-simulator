aa72d054bedd030b6743f863dd1f5cba36542eab
Amay-droid <amaybhardwaj262@gmail.com>
2026-01-06 00:43:04 +0530

    created the processes

--- Core/ProcessManager.cs
+++ Core/ProcessManager.cs
@@ -1,17 +1,53 @@
 using System;
 using System.Linq.Expressions;
-using System.Xml;
+
+
 namespace MiniOS.Core
 {
     public class ProcessManager
     {
         private readonly List<Process> _processTable;
-        private int_nextPid;
+        private int _nextPid;
 
         public ProcessManager()
         {
-            _processTable = New List<process>();
+            _processTable = new List<process>();
             _nextPid =1;
+        }
+
+        Public Process CreateProcess(String name, int priority, int burstTime)
+        {
+            int pid = _nextPid;
+            Process process = new Process(pid,name,priority,burstTime);
+
+            Process.SetState(ProcessState.Ready);
+            _processTable.Add(process);
+
+            Return process;
+
+
+        }
+
+        public void KillProcess(int pid)
+        {
+            foreach (var process  in _processTable)
+            {
+                if(Process.Pid = pid)
+                {
+                    process.SetState(processState.Terminated);
+                    return;
+                }
+            }
+        }
+
+        public List<Process> GetAllProcesses()
+        {
+            return _processTable;
+        }
+
+        public List<Process> GetReadyProcess()
+        {
+            list<process> ReadyProcess = new List<Process>
         }
     }
 }
