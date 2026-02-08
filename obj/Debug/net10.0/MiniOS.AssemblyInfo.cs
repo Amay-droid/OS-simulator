c1964f9b7b961b945fd9d05f9a870a9c9ede5b6e
U-K-06 <utkarsh7khajuria8@gmail.com>
2026-02-08 06:47:41 +0530

    added round robin algo

--- obj/Debug/net10.0/MiniOS.AssemblyInfo.cs
+++ obj/Debug/net10.0/MiniOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c4d95badfca97c9313119fa29ec73882d72572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+929ead2b36011caea177961b4cd24bfcf21ac512")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
